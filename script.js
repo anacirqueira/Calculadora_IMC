05ecf1af74843b1bda48ed1cb5a0f35811302c23
anacirqueira <cirqueiraana6@gmail.com>
2023-08-04 16:25:38 -0300

    Apresentando o resultado do IMC no popup

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
+const modalWrapper = document.querySelector('.modal-wrapper')
+const modalMessage = document.querySelector('.modal .title span')
+const modalBtnClose = document.querySelector('.modal button.close')
+
 
 form.onsubmit = event => {
   event.preventDefault()
@@ -8,5 +12,17 @@ form.onsubmit = event => {
   const weight = inputWeight.value
   const height = inputHeight.value
 
-  console.log(weight, height)
+  const result = IMC(weight, height)
+  const message = `Seu IMC é de ${result}`
+
+  modalMessage.innerText = message
+  modalWrapper.classList.add('open')
+}
+
+modalBtnClose.onclick = () => {
+  modalWrapper.classList.remove('open')
+}
+
+function IMC(weight, height) {
+  return (weight / ((height / 100) ** 2)).toFixed(0)
 }
